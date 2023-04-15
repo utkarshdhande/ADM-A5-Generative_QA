06abfbfe0ba41fbff455b73f13b12edb5d8cbdfa
Utkarsh Dhande <91442197+utkarshdhande@users.noreply.github.com>
2023-04-15 11:51:29 -0400

    Update Streamlit_GenerativeQA.py

--- Streamlit_GenerativeQA.py
+++ Streamlit_GenerativeQA.py
@@ -38,10 +38,10 @@ def retrieve(query):
 
 
     # build our prompt with the retrieved contexts included
-    prompt_start = (
-         "Answer based on the context.\n"
-#          "Context:\n"
-    )
+#     prompt_start = (
+#          "Answer based on the context.\n"
+# #          "Context:\n"
+#     )
     prompt_end = (
         f"\n\nQuestion: {query} \n Answer:"
     )
@@ -49,15 +49,13 @@ def retrieve(query):
     for i in range(1, len(contexts)):
         if len("\n\n---\n\n".join(contexts[:i])) >= limit:
             prompt = (
-                prompt_start +
-                "\n\n---\n\n".join(contexts[:i-1]) +
+
                 prompt_end
             )
             break
         elif i == len(contexts)-1:
             prompt = (
-                prompt_start +
-                "\n\n---\n\n".join(contexts) +
+
                 prompt_end
             )
     return prompt
